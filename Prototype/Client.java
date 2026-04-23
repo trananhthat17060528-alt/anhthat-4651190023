092467fc6d93b16aae2df9bd3156dd73236aecc2
trananhthat17060528-alt <trananhthat17060528@gmail.com>
2026-04-23 11:08:56 +0700

    Add files via upload

---
+++ Prototype/Client.java
@@ -0,0 +1,15 @@
+package Prototype;
+
+public class Client {
+    public static void main(String[] args) {
+        Person person = new Person("Hao");
+        Rabbit rabbit1 = new Rabbit("Holland Lop", "white", 4, person);
+
+        //clone
+        Rabbit rabbit2 = rabbit1.clone();
+        rabbit2.getOwner().setName("Dang Phong Hao");
+
+        System.out.println("Rabbit 1: "+ rabbit1);
+        System.out.println("Rabbit 2: " + rabbit2);
+    }
+}
